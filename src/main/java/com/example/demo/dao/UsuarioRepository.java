3a858e3c244142033e8ff26ddda0d1e084b2d0c6
VicoVillca <vicovillca@hotmail.com>
2022-09-08 23:13:34 -0400

    Update Plsql
    agregamos plsql

--- src/main/java/com/example/demo/dao/UsuarioRepository.java
+++ src/main/java/com/example/demo/dao/UsuarioRepository.java
@@ -10,4 +10,10 @@ import java.util.List;
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     Usuario findByEmail(String email);
     Usuario findByTelefono(String telefono);
+
+    Usuario findByNombre(String nombre);
+    Usuario findByApellido(String apellido);
+
+    @Query("SELECT u FROM Usuario u WHERE u.nombre = :nombre and u.apellido = :apellido")
+    Usuario findUserByStatusAndName( @Param("nombre") Integer nombre,  @Param("apellido") Integer apellido);
 }
